6df9cf08bd86dcdae325d4062274bed4402e5b66
Luana do Vale <108230080+LuanaDoVale@users.noreply.github.com>
2023-01-17 10:18:12 -0300

    Projeto final de estudo em C#
    Projeto final de estudo em C#

---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,81 @@
+﻿using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Parceria;
+using bytebank_ADM.SistemaInterno;
+using bytebank_ADM.Utilitário;
+using System.Collections.Concurrent;
+using System.Security.Cryptography.X509Certificates;
+
+#region
+//Funcionario pedro = new Funcionario("123456789", 2000);
+//pedro.Nome = "Pedro Malazartes";
+
+//Console.WriteLine(pedro.Nome);
+//Console.WriteLine(pedro.GetBonificacao());
+
+//Diretor roberta = new Diretor("987654321");
+//roberta.Nome = "Roberta Silva";
+
+//Console.WriteLine(roberta.Nome);
+//Console.WriteLine(roberta.GetBonificacao());
+
+//GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+//gerenciador.Registrar(pedro);
+//gerenciador.Registrar(roberta);
+//Console.WriteLine("Total de bonificaçoes: "+ gerenciador.TotalDeBonificacao);
+//Console.WriteLine("Total de funcionários: " + Funcionario.TotalDeFuncionarios);
+
+//pedro.AumentarSalario();
+//roberta.AumentarSalario();
+
+//Console.WriteLine("Novo salário do Pedro: " + pedro.Salario);
+//Console.WriteLine("Novo salário da Roberta: " + roberta.Salario);
+#endregion
+
+//CalcularBonificacao();
+UsarSistema();
+void CalcularBonificacao()
+{
+    GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+
+    Designer ulisses = new Designer("123456");
+    ulisses.Nome = "Ulisses Souza";
+
+    Diretor paula = new Diretor("4456456");
+    paula.Nome = "Paula Souza";
+
+    Auxiliar igor = new Auxiliar("84696215");
+    igor.Nome = "Igor Dias";
+
+    GerenteDeConta camila = new GerenteDeConta("55658565");
+    camila.Nome = "Camila Oliveira";
+
+    gerenciador.Registrar(camila);
+    gerenciador.Registrar(igor);
+    gerenciador.Registrar(paula);
+    gerenciador.Registrar(ulisses);
+
+    Console.WriteLine("Total de bonificação = " + gerenciador.TotalDeBonificacao);
+
+}
+
+void UsarSistema()
+{
+    SistemaInterno sistema = new SistemaInterno();
+
+    Diretor ingrid = new Diretor("5465284");
+    ingrid.Nome = "Ingrid Novaes";
+    ingrid.Senha = "123";
+
+    GerenteDeConta ursula = new GerenteDeConta("65413496");
+    ursula.Nome = "Ursula Alcantara";
+    ursula.Senha = "321";
+
+    ParceiroComercial caio = new ParceiroComercial();
+    caio.Senha = "999";
+
+    sistema.Logar(ingrid, "123");
+    sistema.Logar(ursula, "084");
+    sistema.Logar(caio, "999");
+
+}
+
---
+++ bytebank_ADM/Utilitário/GerenciadorDeBonificacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bytebank_ADM.Funcionarios;
+
+namespace bytebank_ADM.Utilitário
+{
+    public class GerenciadorDeBonificacao
+    {
+        public double TotalDeBonificacao { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.TotalDeBonificacao += funcionario.GetBonificacao();
+        }
+    }
+}
